68c8192e6fff828eb4bcf41b529bae1c80e91a0e
Sachit Vithaldas <macintoshsac@gmail.com>
2017-10-22 20:24:57 -0700

    Initial Commit
    Just added some sample code

---
+++ functiontraits.h
@@ -0,0 +1,95 @@
+//
+//  functiontraits.h
+//  Practice Problems
+//
+//  Created by Sachit Vithaldas on 22/10/17.
+//
+//
+
+#ifndef functiontraits_h
+#define functiontraits_h
+
+#pragma once
+
+#include <cstddef>
+#include <tuple>
+
+namespace custom_traits {
+    template<class F>
+    struct function_traits;
+    
+    // function pointer
+    template<class R, class... Args>
+    struct function_traits<R(*)(Args...)> : public function_traits<R(Args...)>
+    {};
+    
+    template<class R, class... Args>
+    struct function_traits<R(Args...)>
+    {
+        using return_type = R;
+        
+        static constexpr std::size_t arity = sizeof...(Args);
+        
+        template <std::size_t N>
+        struct argument
+        {
+            static_assert(N < arity, "error: invalid parameter index.");
+            using type = typename std::tuple_element<N,std::tuple<Args...>>::type;
+        };
+    };
+    
+    // member function pointer
+    template<class R,class C, class... Args>
+    struct function_traits<R(C::*)(Args...)>
+    {
+        using return_type = R;
+        
+        static constexpr std::size_t arity = sizeof...(Args);
+        
+        template <std::size_t N>
+        struct argument
+        {
+            static_assert(N < arity, "error: invalid parameter index.");
+            using type = typename std::tuple_element<N,std::tuple<Args...>>::type;
+        };
+    };
+    
+    // const member function pointer
+    template<class R, class C, class... Args>
+    struct function_traits<R(C::*)(Args...) const> : public function_traits<R(C::*)(Args...)>
+    {};
+    
+    // std::function
+    template<class F>
+    struct function_traits
+    {
+    private:
+        using call_type = function_traits<decltype(&F::operator())>;
+    public:
+        using return_type = typename call_type::return_type;
+        
+        static constexpr std::size_t arity = call_type::arity;
+        
+        template <std::size_t N>
+        struct argument
+        {
+            static_assert(N < arity, "error: invalid parameter index.");
+            using type = typename call_type::template argument<N>::type;
+        };
+    };
+    
+    // Traits
+    // constant member function
+    template<class F>
+    struct is_const_member_function;
+    
+    template<class R, class C, class... Args>
+    struct is_const_member_function<R(C::*)(Args...) const> : public std::true_type
+    {};
+    
+    template<class R, class C, class... Args>
+    struct is_const_member_function<R(C::*)(Args...)> : public std::false_type
+    {};
+}
+
+#endif /* functiontraits_h */
---
+++ main.cpp
@@ -0,0 +1,58 @@
+//
+//  main.cpp
+//  FunctionTraits
+//
+//  Created by Sachit Vithaldas on 22/10/17.
+//
+//
+
+#include <iostream>
+#include <type_traits>
+#include <functional>
+
+#include "functiontraits.h"
+
+class Example
+{
+public:
+    int exampleFn1(int&& a, int const& b, int c) const
+    {
+        return 5;
+    }
+    int operator()(int const a) const
+    {
+        return 5;
+    }
+    
+    void blank()
+    {
+        return;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    using namespace custom_traits;
+    using namespace std;
+    
+    std::function<int(double)> test;
+    
+    static_assert(is_const_member_function<decltype(&Example::exampleFn1)>::value, "");
+    static_assert(is_const_member_function<decltype(&Example::operator())>::value, "");
+    
+    using E1 = function_traits<decltype(&Example::exampleFn1)>;
+    static_assert(is_same<E1::return_type, int>(), "");
+    static_assert(E1::arity == 3, "");
+    static_assert(std::is_same<E1::argument<0>::type,int&&>::value,"");
+    static_assert(std::is_rvalue_reference<E1::argument<0>::type>::value,"");
+    
+    using E2 = function_traits<decltype(&Example::blank)>;
+    static_assert(E2::arity == 0, "");
+    
+    using E3 = function_traits<decltype(test)>;
+    static_assert(is_same<E3::return_type, int>(), "");
+    static_assert(is_floating_point<E3::argument<0>::type>::value, "");
+    
+    return 0;
+}
